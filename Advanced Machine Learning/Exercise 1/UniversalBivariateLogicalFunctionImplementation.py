8263df7baed245545ab7041cf2f20c1659f10f80
darrennoronha75 <darrennoronha1981@gmail.com>
2024-12-23 18:27:39 +0100

    And So We Start

--- Advanced Machine Learning/Exercise 1/UniversalBivariateLogicalFunctionImplementation.py
+++ Advanced Machine Learning/Exercise 1/UniversalBivariateLogicalFunctionImplementation.py
@@ -1,1 +1,12 @@
-print("Hello")
+import numpy as np
+
+# Define the input matrix for a logical bivariate function
+# Each column represents a different input combination
+x = np.array([
+    [0, 0, 1, 1],
+    [0, 1, 0, 1]
+])
+
+print(x)
+
+# Define the weights for the logical bivariate function
