c94a105dae149603b587e74b78d7d4b73a6c0a79
darrennoronha75 <darrennoronha1981@gmail.com>
2024-12-23 19:33:03 +0100

    ANDwithNOHiddenLayer added

--- Advanced Machine Learning/Exercise 1/UniversalBivariateLogicalFunctionImplementation.py
+++ Advanced Machine Learning/Exercise 1/UniversalBivariateLogicalFunctionImplementation.py
@@ -4,9 +4,7 @@ import numpy as np
 
 # Define a function that returns the affine linear combination of the input matrix and the weights plus the bias
 def affine_linear_combination(x, w, b):
-    e = np.ones((1,4))
-    print(b.shape)
-    print(e.shape)
+    e = np.ones((1,x.shape[1]))
     return w.T @ x + b.T @ e
 
 # Define ReLU activation function
@@ -21,6 +19,8 @@ x = np.array([
 ])
 
 # Layer 1 - Universal Layer to identify Points
+#----------------------------------------------
+
 # Define the weights and bias for the first layer
 w1 = np.array([[-1, -1, 1, 1],
               [-1, 1, -1, 1]])
@@ -36,3 +36,24 @@ activation1 = ReLU(out1)
 
 # Print the activation of the first layer
 print(activation1)
+
+# Layer 2 - Layer to Map Points to Logical Functions
+#---------------------------------------------------
+
+# Define the weights and bias for the second layer
+w2 = np.array([[0, 0, 0],
+               [0, 1, 1],
+               [0, 1, 1],
+               [1, 1, 0]])
+
+b2 = np.array([[0, 0, 0]])
+
+# Compute the output of the second layer
+out2 = affine_linear_combination(activation1, w2, b2)
+print(out2)
+
+# Apply the ReLU activation function to output of the second layer
+activation2 = ReLU(out2)
+
+# Print the activation of the second layer
+print(activation2)
