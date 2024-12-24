2196cffaae70b3d9e65419d85a38f478f4510242
darrennoronha75 <darrennoronha1981@gmail.com>
2024-12-24 11:59:41 +0100

    Two Activation Functions added

---
+++ Advanced Machine Learning/aml/Exercise 3/amllib/activations/__init__.py
@@ -0,0 +1,17 @@
+"""
+Activation function package of the neural network library
+for "Advanced Machine Learning".
+"""
+
+__author__ = "Jens-Peter M. Zemke, Jonas Grams"
+__version__ = "1.1"
+
+__name__ = "amllib.activations"
+__package__ = "amllib.activations"
+
+from .activation import Activation
+
+# Import ReLU activation function
+from .relu import ReLU
+from .leaky_relu import LeakyReLU
+from .modified_heaviside import ModifiedHeaviside
